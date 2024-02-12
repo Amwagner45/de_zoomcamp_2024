b5379d398a83e68584835db67525f54e97492bd9
Alexander Michael Wagner <amwagner45@gmail.com>
2024-02-11 23:09:38 -0500

    ingested nyc_taxidata to duckdb

---
+++ 4_Analytics_Engineering/ingestion.py
@@ -0,0 +1,54 @@
+import duckdb
+
+
+def collect_urls(year: str, color: str):
+    for month in range(1, 13):
+        if month < 10:
+            month = "0" + str(month)
+        else:
+            month
+
+        url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/{color}_tripdata_{year}-{month}.parquet"
+
+        print(url)
+
+
+def create_table(conn, color, year):
+    conn.execute(
+        f""" create or replace table {color}_tripdata_{year} as 
+            SELECT * FROM read_parquet('https://d37ci6vzurychx.cloudfront.net/trip-data/{color}_tripdata_{year}-01.parquet')
+            union all
+            SELECT * FROM read_parquet('https://d37ci6vzurychx.cloudfront.net/trip-data/{color}_tripdata_{year}-02.parquet')
+            union all
+            SELECT * FROM read_parquet('https://d37ci6vzurychx.cloudfront.net/trip-data/{color}_tripdata_{year}-03.parquet')
+            union all
+            SELECT * FROM read_parquet('https://d37ci6vzurychx.cloudfront.net/trip-data/{color}_tripdata_{year}-04.parquet')
+            union all
+            SELECT * FROM read_parquet('https://d37ci6vzurychx.cloudfront.net/trip-data/{color}_tripdata_{year}-05.parquet')
+            union all
+            SELECT * FROM read_parquet('https://d37ci6vzurychx.cloudfront.net/trip-data/{color}_tripdata_{year}-06.parquet')
+            union all
+            SELECT * FROM read_parquet('https://d37ci6vzurychx.cloudfront.net/trip-data/{color}_tripdata_{year}-07.parquet')
+            union all
+            SELECT * FROM read_parquet('https://d37ci6vzurychx.cloudfront.net/trip-data/{color}_tripdata_{year}-08.parquet')
+            union all
+            SELECT * FROM read_parquet('https://d37ci6vzurychx.cloudfront.net/trip-data/{color}_tripdata_{year}-09.parquet')
+            union all
+            SELECT * FROM read_parquet('https://d37ci6vzurychx.cloudfront.net/trip-data/{color}_tripdata_{year}-10.parquet')
+            union all
+            SELECT * FROM read_parquet('https://d37ci6vzurychx.cloudfront.net/trip-data/{color}_tripdata_{year}-11.parquet')
+            union all
+            SELECT * FROM read_parquet('https://d37ci6vzurychx.cloudfront.net/trip-data/{color}_tripdata_{year}-12.parquet')
+            ;
+        """
+    )
+
+
+conn = duckdb.connect(database="ny_taxi.db", read_only=False)
+create_table(conn, "green", "2020")
+
+conn.execute("show tables;")
+print(conn.fetchall())
+
+conn.execute("select * from green_tripdata_2020 limit 10;")
+print(conn.fetchone())
